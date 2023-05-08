ea50fdf02c3fdb194df8dafbaf7b002a0f759614
Vinicius Evair <vsilva.evair@gmail.com>
2023-05-08 15:05:12 -0300

    Update indent line configuration

--- nvim/after/plugin/indentLine.lua
+++ nvim/after/plugin/indentLine.lua
@@ -1,4 +1,11 @@
 require("indent_blankline").setup {
+    char = "│",
     show_current_context = true,
+    show_end_of_line = false,
     show_current_context_start = false,
+    show_trailing_blankline_indent = false,
+    use_treesitter = true,
+    --filetype_exclude = {},
+    --vim.api.nvim_set_hl(0, "IndentBlankLineContextChar", {fg = "#FFCCFF", bg = "none"}),
+    --vim.api.nvim_set_hl(0, "IndentBlankLineChar", {fg = "#666666", bg = "none"})
 }
