7be24c3c81c7c107fa95cabd3aafeaac8d9b9080
Vinicius Evair <vsilva.evair@gmail.com>
2023-05-18 11:54:09 -0300

    Changed keymaps and documentation border

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -29,43 +29,9 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = {
-        buffer = bufnr,
-        remap = false
-    }
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-end)
-
 lsp.setup()
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-n>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-p>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-
--- disable completion with tab
--- this helps with copilot setup
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
 local lspkind = require('lspkind')
 
 local cmp_config = lsp.defaults.cmp_config({
@@ -80,8 +46,15 @@ local cmp_config = lsp.defaults.cmp_config({
                 "—",
                 "◈",
                 " ",
-            }
+            },
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
         }),
+
+        documentation = cmp.config.window.bordered({
+            border = "single",
+        })
     },
 
     sources = {
@@ -92,7 +65,7 @@ local cmp_config = lsp.defaults.cmp_config({
         {name = "nvim_lua", keyword_length = 2},
     },
     formatting = {
-        fields = {"menu", "abbr", "kind"},
+        fields = { "abbr", "kind", "menu" },
         format = lspkind.cmp_format({
             maxwidth = 20,
             ellipsis_char = '...',
@@ -107,7 +80,15 @@ local cmp_config = lsp.defaults.cmp_config({
                 end
                 vim_item.menu = ""
                 return vim_item
-            end
+            end,
+            mode = "symbol_text",
+            menu = ({
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[LuaSnip]",
+                nvim_lua = "[Lua]",
+                latex_symbols = "[Latex]",
+            })
         }),
     },
 
@@ -116,7 +97,7 @@ local cmp_config = lsp.defaults.cmp_config({
             cmp.config.compare.exact,
             cmp.config.compare.locality,
         }
-    }
+    },
 })
 
 cmp.setup(cmp_config)
