d22546889e9e2f5c356abee9035fcca1fd742bde
Vinicius Evair <vsilva.evair@gmail.com>
2023-05-30 20:32:47 -0300

    Changed some lsp shortcuts

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -98,6 +98,15 @@ local cmp_config = lsp.defaults.cmp_config({
             cmp.config.compare.locality,
         }
     },
+
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<tab>'] = nil,
+        ['S-tab>'] = nil,
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
 })
 
 cmp.setup(cmp_config)
